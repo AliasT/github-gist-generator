14fa6e214005b4538d3c0a13d8b2ea45d31a4778
chaixb <1481569358@qq.com>
2019-09-07 18:33:35 +0800

    fetch content

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,9 @@
-use std::default;
+use std::collections::HashMap;
 use std::error::Error;
-use url::{ParseError, Url};
+use url::Url;
+extern crate reqwest;
+use base64::{decode, encode};
+use serde::Deserialize;
 
 #[derive(Debug, Default)]
 pub struct Gist {
@@ -12,12 +15,26 @@ pub struct Gist {
     end: i32,
 }
 
+/// Github file content response
+#[derive(Deserialize)]
+pub struct ContentResponse {
+    name: String,
+    content: String,
+}
+
+pub fn get_content(link: &str) -> Result<ContentResponse, Box<dyn Error>> {
+    let res: ContentResponse = reqwest::get(link)?.json()?;
+    println!("{:?}", res.content);
+    Ok(res)
+}
+
 /// 根据github文件地址生成gist
 /// includes: line range
 pub fn generate(link: &str) -> &str {
     return "gist url";
 }
 
+/// 46d5465b35188ddf03bd884220b1bb5934461bf2
 /// Example url: https://github.com/AliasT/public/blob/master/lib/react.cjs.js#L14-L16
 ///                                | user | repo |   refer   |        path     |
 ///
@@ -25,7 +42,8 @@ impl Gist {
     pub fn parse(link: &str) -> Result<Option<Gist>, Box<dyn Error>> {
         let file_url_object = Url::parse(link)?;
         let path = file_url_object.path();
-        let re = regex::Regex::new(r"/(?P<user>.+?)/(?P<repo>.+?)/blob/(?P<refer>.+?)/(?P<file>.+)$")?;
+        let re =
+            regex::Regex::new(r"/(?P<user>.+?)/(?P<repo>.+?)/blob/(?P<refer>.+?)/(?P<file>.+)$")?;
         let gist = match re.captures(path) {
             Some(caps) => Some(Gist {
                 user: String::from(caps.name("user").unwrap().as_str()),
@@ -42,19 +60,26 @@ impl Gist {
         Ok(gist)
     }
 
-    pub create(&self) {
-
-    }
+    pub fn create(&self) {}
 }
 
 #[test]
 fn test_parse() {
     if let Some(gist) =
-        Gist::parse("https://github.com/AliasT/public/blob/master/lib/react.cjs.js#L14-L16").unwrap()
+        Gist::parse("https://github.com/AliasT/public/blob/master/lib/react.cjs.js#L14-L16")
+            .unwrap()
     {
         assert!(gist.user == "AliasT");
         assert!(gist.repo == "public");
         assert!(gist.refer == "master");
         assert!(gist.path == "lib/react.cjs.js");
     }
+}
+
+#[test]
+fn test_get_content() {
+    let res =
+        get_content("https://api.github.com/repos/aliast/online-ide-discovery/contents/README.md")
+            .unwrap();
+    assert!(res.name == "README.md")
 }
