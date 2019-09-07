c5863b6a622375c77eaf7df6f48e323b25028a61
chaixb <1481569358@qq.com>
2019-09-07 14:17:17 +0800

    layout

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
 fn main() {
     println!("Hello, world!");
 }
+
+struct Gist {
+    user: String,
+    repo: String,
+    refer: String,
+    path: String,
+    start: i32,
+    end: i32,
+}
+
+/// 根据github文件地址生成gist
+/// includes: line range
+fn generate(url: &str) -> &str {
+    return "gist url";
+}
+
+/// Example url: https://github.com/AliasT/public/blob/master/lib/react.cjs.js#L14-L16
+///                                |:user | :repo |    :ref  |      :path     |
+fn parse(url: &str) -> Option<Gist> {
+    return None;
+}
